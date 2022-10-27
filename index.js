ccf7eba490c91a58538ee555a949e7be65d9342f
Fahim <KhanFahimFaisal@outlook.com>
2022-10-27 18:20:14 +0600

    added comments

--- index.js
+++ index.js
@@ -4,13 +4,19 @@ const cors = require("cors");
 const port = 5000;
 const courses = require("./data.json");
 app.use(cors());
+
+//configuring static files
 app.use("/images", express.static("images"));
 app.get("/", (req, res) => {
   res.send("Server Working");
 });
+
+//Get all course data
 app.get("/allCourses", (req, res) => {
   res.send(courses);
 });
+
+// get a specific course data
 app.get("/:id", (req, res) => {
   const requestedId = +req.params.id;
   const courseToSend = courses.find((course) => {
@@ -19,6 +25,7 @@ app.get("/:id", (req, res) => {
   res.send(courseToSend);
 });
 
+// Start the app with following message
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
 });
